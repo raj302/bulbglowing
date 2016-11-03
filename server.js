9400d4926215aa932fd36a456feb4b50a59b9aa8
rajeev ranjan <becoolrajeev@gmail.com>
2016-11-04 01:28:00 +0530

    save message

--- server.js
+++ server.js
@@ -62,103 +62,12 @@ function off() {
 }
 
 
-//var gpio=require("pi-gpio");
-//gpio.open(16,"output", function(err){
-   //gpio.write(16,1,function(){
-     //gpio.close(16);
-   //});
-  //});
-
-
-// python code execution starts here
-//this code takes time to execute. Try another if possible. name is python code 1
 var PythonShell = require('python-shell');
 
 var options = {
   mode: 'text',
-  //pythonPath: '/usr/lib/python2.7',
-  //pythonOptions: ['-u'],
    args: [30]
 };
-
-
-
- // PythonShell.run('test01.py', options, function (err, results) {
- //   if (err) throw err;
- //   // results is an array consisting of messages collected during execution
- //   console.log("finished executing python script");
- //   console.log(results);
- // });
-
-
-//execution of python code 2 starts here. Its not tested yet.
-// python code 2 starts here with comment.
-// var options = {
-//   mode: 'text',
-//   pythonPath: 'C:\Users\rajeev\AppData\Local\Programs\Python\Python35-32',
-//   pythonOptions: ['-u'],
-//   scriptPath: 'F:\website\ipl5th_sem_working'
-//   // args: ['value1', 'value2', 'value3']
-// };
-
-// PythonShell.run('my_script.py', options, function (err, results) {
-//   if (err) throw err;
-//   // results is an array consisting of messages collected during execution
-//   console.log('results: %j', results);
-// });
-
-
-// PythonShell.run('test.py', function (err) {
-//   if (err) throw err;
-//   console.log('finished');
-// });
-//execution of python code 2 ends here.
-
-//var spawn = require("child_process").spawn;
-
-// // On Windows Only ...
-// const spawn = require('child_process').spawn;
-// const bat = spawn('cmd.exe', ['/c', 'my.bat']);
-
-// bat.stdout.on('data', (data) => {
-//   console.log(data);
-// });
-
-// bat.stderr.on('data', (data) => {
-//   console.log(data);
-// });
-
-// bat.on('exit', (code) => {
-//   console.log(`Child exited with code ${code}`);
-// });
-
-// // On Windows Only ...
-// const spawn = require('child_process').spawn;
-// const bat = spawn('cmd.exe', ['/c', 'my.bat']);
-
-// bat.stdout.on('data', (data) => {
-//   console.log(data);
-// });
-
-// bat.stderr.on('data', (data) => {
-//   console.log(data);
-// });
-
-// bat.on('exit', (code) => {
-//   console.log(`Child exited with code ${code}`);
-// });
-
-// const spawn = require('child_process').spawn;
-
-// const child = spawn(process.argv[0], ['test.py'], {
-//   detached: true,
-//   stdio: 'ignore'
-// });
-
-// child.unref();
-
-// var db = mongojs('mongodb://dev.frugaltek.com:27017/flms', ['register']);
- // var db = mongojs('mongodb://bhuvanesh:123@ds023398.mlab.com:23398/heroku_461p1j1s', collections);
 var port = Number(process.env.PORT || 3000)
 var session = require('express-session');
 app.use(session({
