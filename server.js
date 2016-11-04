1d0ecb32a14213dbae85fcdeec0df6fe2365423a
rajeev ranjan <becoolrajeev@gmail.com>
2016-11-04 21:13:43 +0530

    save message

--- server.js
+++ server.js
@@ -33,34 +33,6 @@ var gpio = require('rpi-gpio');
 
 
 var gpio = require('rpi-gpio');
- 
-var pin   = 10;
-var delay = 200;
-var count = 0;
-var max   = 20;
- 
-gpio.setup(pin, gpio.DIR_OUT, on);
- 
-function on() {
-    if (count >= max) {
-        gpio.destroy(function() {
-            console.log('Closed pins, now exit');
-        });
-        return;
-    }
- 
-    setTimeout(function() {
-        gpio.write(pin, 1, off);
-        count += 1;
-    }, delay);
-}
- 
-function off() {
-    setTimeout(function() {
-        gpio.write(pin, 0, on);
-    }, delay);
-}
-
 
 var PythonShell = require('python-shell');
 
